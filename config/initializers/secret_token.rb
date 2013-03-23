71fe9e344b2b574692b11e76aa143170786b38cd
Ben Gibbs <ben@bobop.co.uk>
2013-03-23 11:39:22 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GtfsApi::Application.config.secret_token = '6d03670e40dfc830581a19165a043caa22b1f22657979eedbe3a1fdd1981a69a7e87154983799a5d9d385615d10945c59dfd721b28d4cc7c0748b5245a5b95eb'
---
+++ lib/tasks/import_gtfs.rake
@@ -0,0 +1,9 @@
+desc "Imports GTFS to DB"
+task :import_gtfs do
+  puts "About to Import GTFS zip"
+  source = GTFS::Source.build('http://store.datagm.org.uk/sets/tfgm/tfgmgtfs.zip')
+  puts "Imported GTFS zip"
+  source.routes.each do |r|
+    puts r
+  end
+end
