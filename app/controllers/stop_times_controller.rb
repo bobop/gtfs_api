1347fdf6a433ed0ea00a7695f00009677ff3e4d8
Ben Gibbs <ben@bobop.co.uk>
2013-03-24 08:32:25 +0000

    added trip_ids param to stop_times

--- app/controllers/stop_times_controller.rb
+++ app/controllers/stop_times_controller.rb
@@ -8,6 +8,10 @@ class StopTimesController < ApplicationController
     else
       @stop_times = @stop_times.limit(200)
     end
+    # params[:trip_ids]=2222,2345,5432,6543
+    if params[:trip_ids].present?
+      @stop_times = @stop_times.where(trip_id: params[:trip_ids])
+    end
     # @stop.each.map {|t| t.include_trip = true} if params[:include_trip] == 'true'
 
     render json: @stop_times
