ea79e4fbdf19a8f88bd1f1e02fb2f15285a7db0f
Ben Gibbs <ben@bobop.co.uk>
2013-03-24 11:27:32 +0000

    added distinct param for routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,20 +2,23 @@ class RoutesController < ApplicationController
   respond_to :json
 
   def index
-    @routes = Route.order(:id)
+    # @routes = Route.order(:id)
+    # LIMIT RESULTS
+    @routes = Route.limit(20) if !params[:limit].present?
+    @routes = Route.limit(params[:limit]) if params[:limit].present?
+
     if params[:name].present?
       @routes = @routes.where("LOWER(route_short_name) LIKE '%#{params[:name].downcase}%' OR LOWER(route_long_name) LIKE '%#{params[:name].downcase}%'")
     end
     if params[:agency_id].present?
       @routes = @routes.where(agency_id: Agency.where(agency_id: params[:agency_id]).first.id)
     end
-    
-    # LIMIT RESULTS
-    @routes = @routes.limit(20) if !params[:limit].present?
-    @routes = @routes.limit(params[:limit]) if params[:limit].present?
 
     @routes.each.map {|r| r.include_trips = true} if params[:include_trips] == 'true'
-    logger.info "--- @routes = #{@routes.inspect}"
+    
+    if params[:distinct].present?
+      @routes = @routes.select("DISTINCT ON (route_long_name) routes.*").order("route_long_name DESC")
+    end
 
     if params[:callback].present?
       render json: @routes, callback: params[:callback]
